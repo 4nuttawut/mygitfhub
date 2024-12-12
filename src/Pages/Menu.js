01e515c4674bb0295aaa595fd9e458b6ab19e199
4nuttawut <glong.nuttawut2003@gmail.com>
2024-12-12 09:49:40 +0700

    frist comit

---
+++ src/Pages/Menu.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+function Menu() {
+    return(
+        <div class="card-group">
+        <div class="card">
+          <img src="https://static.amarintv.com/media/PJVlR0ljpN9mLUt8dX1IcpaZekNXYQxflBLkSnKt7KxRhzmA76d4Rs60cLWUdHz5u9.jpg" class="card-img-top" alt="..."/>
+          <div class="card-body">
+            <h5 class="card-title">ข้าวหมกไก่</h5>
+            <p class="card-text">“ข้าวหมกไก่” กับเอกลักษณ์ของรสชาติที่มีความสมดุลระหว่างรสเปรี้ยว เผ็ด เค็ม หวาน</p>
+          </div>
+          <div class="card-footer">
+            <small class="text-muted">Last updated 3 mins ago</small>
+          </div>
+        </div>
+        <div class="card">
+          <img src="https://img.wongnai.com/p/1920x0/2024/05/20/8b6b33ca3413465b80c0b37cc847ef9b.jpg" class="card-img-top" alt="..."/>
+          <div class="card-body">
+            <h5 class="card-title">ผัดไทยกุ้ง</h5>
+            <p class="card-text">"ผัดไทยกุ้ง"</p>
+          </div>
+          <div class="card-footer">
+            <small class="text-muted">Last updated 3 mins ago</small>
+          </div>
+        </div>
+        <div class="card">
+          <img src="https://www.maggi.co.th/sites/default/files/styles/home_stage_944_531/public/srh_recipes/9f922d8fe6344f4a8b641ebd716be4cd.jpg?h=4f5b30f1&itok=KRUdjO7X.jpg" class="card-img-top" alt="..."/>
+          <div class="card-body">
+            <h5 class="card-title">"ข้าวผัดกุ้ง"</h5>
+            <p class="card-text">ข้าวผัด เป็นอาหารไทยประเภทข้าวผัด เป็นการนำข้าวสวยลงไปผัดคลุกกับซอส น้ำพริก หรือเครื่องปรุงรสต่าง ๆ เพื่อให้ได้รสชาติ</p>
+          </div>
+          <div class="card-footer">
+            <small class="text-muted">Last updated 3 mins ago</small>
+          </div>
+        </div>
+      </div>
+    );
+}
+
+export default Menu;
